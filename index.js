7af1f728d37f8ba2394e584e7c05c97bd4c9a7fd
Carl Steele <casteeleu@gmail.com>
2018-08-15 23:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = '03680f182807b2105be583b70dad3a65a1cef68f'
 
 function handleFormSubmit(event) {
-  //handle submit event
+  event.preventDefault();
+  // select the element with an id of "city"
+  // get the value of that element, set a variable city = that value
+  // fetchCurrentWeather(city)
+  
+  let city = document.getElementById("city").value
+  fetchCurrentWeather(city)
 }
 
 function fetchCurrentWeather(city) {
-  //fetch current weather based on city
+  // https://api.openweathermap.org/data/2.5/weather?q=THECITY&APPID=your api key&units=imperial
+  // `hi ${city}`
+  // "hi " + city
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=city&APPID=03680f182807b2105be583b70dad3a65a1cef68fkey&units=imperial")
+  .then(response => response.json())
+  .then(displayCurrentWeather)
+
 }
 
 function displayCurrentWeather(json) {
+  let temp = weatherJson.main.temp
+  let tempRow = document.getElementById("temp")
   //render current weather data to the DOM using provided IDs and json from API
 }
 
@@ -26,5 +40,10 @@ function createChart(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  // alert("Form Submitted!") 
+  
+  let aThing = document.getElementById("cityForm");
+  aThing.addEventListener("submit", handleFormSubmit)
   //add event listener here for form submission
+  
 })
